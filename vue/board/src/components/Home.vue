42013b8037384845a5fa43c5fee1d74e7bd5bd8d
kei <k10mail66@gmail.com>
2020-12-12 03:54:42 +0900

    v5

---
+++ vue/board/src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Home!<br>
+    <router-link to="/login">LoginPage</router-link><br>
+    <router-link to="/regist">RegistPage</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
---
+++ vue/board/src/components/MyIndex.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    welcome, {{ this.$route.params.name }}!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyIndex'
+}
+</script>
